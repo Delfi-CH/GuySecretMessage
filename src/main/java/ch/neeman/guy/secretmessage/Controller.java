02ddeccda312e9c09bf3a62049c29ff9afca7491
Guy Neeman <guyneeman07@gmail.com>
2026-05-11 15:43:13 +0200

    refactor: remove /api

--- src/main/java/ch/neeman/guy/secretmessage/Controller.java
+++ src/main/java/ch/neeman/guy/secretmessage/Controller.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/secretmessage")
+@RequestMapping("/secretmessage")
 @CrossOrigin("*")
 public class Controller {
 
