b0a93b1c921330a47a8a122274c7ae216172c398
Guy Neeman <guyneeman07@gmail.com>
2026-05-14 23:37:36 +0200

    refactor: Bring back /Api

--- src/main/java/ch/neeman/guy/secretmessage/Controller.java
+++ src/main/java/ch/neeman/guy/secretmessage/Controller.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/secretmessage")
+@RequestMapping("/api/secretmessage")
 @CrossOrigin("*")
 public class Controller {
 
